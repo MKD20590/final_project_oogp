70ea8d2a5cf0eaa7a20ae9067291225739da1b06
MKD20590 <michelleandreas123@gmail.com>
2025-02-03 12:57:36 +0700

    tes

--- Assets/Apps/Scripts/MenuManager.cs
+++ Assets/Apps/Scripts/MenuManager.cs
@@ -7,7 +7,7 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //tes
+        
     }
 
     // Update is called once per frame
